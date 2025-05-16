89d7ad3a6b31ae931768550f8ffdf79d33a15d3b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-16 20:43:26 -0300

    finalizar o programa

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿﻿﻿double altura, PesoIdeal;
+string sexo;
+
+Console.Write("Digite a sua respectiva altura em Metros: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Sexo Masculino = [M] / Feminino = [F]: ");
+sexo = Console.ReadLine()!.ToUpper().Trim();
+
+if (sexo != "M" && sexo != "F")
+{
+    Console.WriteLine("Não possuo fórmula para cálculo");
+}
+else
+{
+    if (sexo == "M")
+    {
+        PesoIdeal = altura * 72.7 - 58.0;
+    }
+    else
+    {
+        PesoIdeal = altura * 62.1 - 44.7;
+    }
+
+    Console.WriteLine($"Seu peso ideal é {PesoIdeal:N1}kg.");
+
+}
+
